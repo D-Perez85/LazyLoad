bca87ce69b4ba736d6c2af0c7090f590a82beb91
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-04-15 19:15:57 -0300

    LazyLoad Basico

--- src/01-lazyload/pages/LazyPage2.tsx
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -1,8 +1,4 @@
-import React from 'react'
-
 export const LazyPage2 = () => {
-  return (
-<h1> LazyPage2 </h1>
-  )
-}
- 
+  return <h1> LazyPage2 </h1>;
+};
+export default LazyPage2;
--- src/01-lazyload/pages/LazyPage3.tsx
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -1,8 +1,4 @@
-import React from 'react'
-
 export const LazyPage3 = () => {
-  return (
-<h1> LazyPage3 </h1>
-  )
-}
- 
+  return <h1> LazyPage3 </h1>;
+};
+export default LazyPage3;
